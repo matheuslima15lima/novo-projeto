f9ad7e7cdc86b66ec2f644eff2b350b980c96bc2
Matheus Lima Cavalcanti <46447568812@info.local>
2023-03-30 14:56:40 -0300

    adicionado backend

---
+++ backend -l/Program.cs
@@ -0,0 +1,69 @@
+﻿// //variaveis
+
+// //declarando variavel
+// //tipo nome = valor
+// string nome = "Matheus";
+// const int idade = 15;
+// Console.WriteLine("a idade do " + nome + " e " + idade + " anos");
+ 
+//tipos de dados
+// int quantidade = 10;
+// double preço = 4.99d;
+// float altura = 1.80f;
+// bool careca = true;
+// string texto = "Ola mundo";
+// char letra = 'C';
+
+// //operador de atribuição
+// int idade = 36;
+
+// operadores aritmeticos
+
+// //soma
+// Console.WriteLine(4 + 4);
+
+
+// // subtracao
+// Console.WriteLine(10 - 8);
+
+
+// // modulo
+// Console.WriteLine(5 % 2);
+
+
+//igual a
+// Console.WriteLine(5 == 6);
+
+
+// Maior ou igual a 
+
+
+//  Menor ou igual a
+
+//operadores logicos e tabela verdade
+
+//&& : e
+//|| : ou
+// ! : negacao
+// Console.WriteLine(5 == 5 && 8 == 8);
+// Console.WriteLine(5 == 5 && 8 == 9);
+// Console.WriteLine(5 == 6 && 8 == 8);
+
+// Console.WriteLine(2 == 2 || 3 == 4);
+// Console.WriteLine(2 == 1 || 3 == 4);
+// Console.WriteLine(!(2 == 4 || 3 == 8));
+
+//crie um programa para calcular o imc de uma pessoa
+
+//entradas
+string nome = "Matheus";
+float peso = 58.8f;
+float altura = 1.75f;
+
+//processamento
+float imc = peso / (altura * altura);
+Console.WriteLine("O IMC do " + nome + " e de : " + Math.Round (imc,2));
+
+
+
+
---
+++ projeto-idade/Program.cs
@@ -0,0 +1,27 @@
+﻿
+// Console.WriteLine($"Informe a sua idade: ");
+// int idade = int.Parse(Console.ReadLine());
+
+// int mes = idade * 12;
+// int dias = idade * 365;
+// int horas = dias * 24;
+// int minutos = horas * 60;
+
+// Console.WriteLine($"A idade em mes e : {mes}, em dias e igual a : {dias}, em horas igual a : {horas} e em minuto igual a {minutos}");
+
+
+
+
+Console.WriteLine($"informe sua ano de nascimento");
+int anoNascimento = int.Parse(Console.ReadLine());
+
+int anoAtual = DateTime.Now.Year;
+
+int idade = anoAtual - anoNascimento;
+
+Console.WriteLine($"a idade e {idade} anos");
+
+
+
+
+
