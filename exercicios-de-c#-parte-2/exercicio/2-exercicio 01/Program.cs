326a0ca57239a52f3e3f34c282b15b6190e0e822
Matheus Lima Cavalcanti <46447568812@info.local>
2023-04-04 17:13:54 -0300

    adiconado projetos

---
+++ exercicios-de-c#-parte-2/exercicio/2-exercicio 01/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.WriteLine($"escreva o seu dia de nascimento");
+ int dia = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine($"escreva o seu mes de nascimento");
+int mes = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine($"escreva o seu ano de nascimento");
+int ano = int.Parse(Console.ReadLine());
+
+if (dia > 31)
+{
+    Console.WriteLine($"erro data falsa");
+    
+}
+
+else if ( dia < 1)
+{
+    Console.WriteLine($"erro data falsa");
+    
+}
+if(mes > 12 )
+{
+    Console.WriteLine($"erro data falsa");
+    
+}
+
+if (ano > 2013)
+{
+    Console.WriteLine($"erro data falsa");
+    
+}
+
+if (dia <= 31 && mes <= 12 && ano <= 2013)
+{
+ Console.WriteLine($"data cadastrada!");
+    
+}
+
---
+++ exercicios-de-c#-parte-2/exercicio/2-exercicio 02/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine($"escreva os numeros");
+ int numero1 = int.Parse(Console.ReadLine()) ;
+
+ int numero2 = int.Parse(Console.ReadLine()) ;
+
+ int numero3 = int.Parse(Console.ReadLine()) ;
+
+ if (numero1 > numero2 && numero1 > numero3)
+{
+    Console.WriteLine($"numero1 maior");
+    
+}
+if (numero2 > numero1 && numero2 == numero3)
+{
+    Console.WriteLine($"numero2 e numero3 maiores");
+    
+}
+if (numero2 > numero1 && numero2 > numero3);
+{
+    Console.WriteLine($"numero 2 maior");
+    
+}
+if (numero2 > numero3 && numero2 == numero1)
+{
+    Console.WriteLine($"numero2 e numero1 maiores");
+    
+
+}
+
+
+else if (numero1 == numero2 && numero1 == numero3)
+{ 
+    Console.WriteLine($"todos iguais");
+    
+}
