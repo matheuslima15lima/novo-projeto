78d2fabfd4b08c89a8e8dd55d9a2579dc5ab06ba
Matheus Lima Cavalcanti <46447568812@info.local>
2023-04-10 17:22:40 -0300

    adicionado isso

--- exercicios-06-04-23/exercicio-04/Program.cs
+++ exercicios-06-04-23/exercicio-04/Program.cs
@@ -1,70 +1,141 @@
-﻿Console.WriteLine($"qual seu sexo? M/F");
-char resposta1 = char.Parse(Console.ReadLine());
+﻿// Console.WriteLine($"qual seu sexo? M/F");
+// char resposta1 = char.Parse(Console.ReadLine());
 
-Console.WriteLine($"qual seu sexo? M/F");
-char resposta2 = char.Parse(Console.ReadLine());
+// Console.WriteLine($"qual seu sexo? M/F");
+// char resposta2 = char.Parse(Console.ReadLine());
 
-Console.WriteLine($"qual seu sexo? M/F");
-char resposta3 = char.Parse(Console.ReadLine());
+// Console.WriteLine($"qual seu sexo? M/F");
+// char resposta3 = char.Parse(Console.ReadLine());
 
-Console.WriteLine($"qual seu sexo? M/F");
-char resposta4 = char.Parse(Console.ReadLine());
+// Console.WriteLine($"qual seu sexo? M/F");
+// char resposta4 = char.Parse(Console.ReadLine());
 
-Console.WriteLine($"qual seu sexo? M/F");
-char resposta5 = char.Parse(Console.ReadLine());
+// Console.WriteLine($"qual seu sexo? M/F");
+// char resposta5 = char.Parse(Console.ReadLine());
 
-Console.WriteLine($"qual seu sexo? M/F");
-char resposta6 = char.Parse(Console.ReadLine());
+// Console.WriteLine($"qual seu sexo? M/F");
+// char resposta6 = char.Parse(Console.ReadLine());
 
-Console.WriteLine($"qual seu sexo? M/F");
-char resposta7 = char.Parse(Console.ReadLine());
+// Console.WriteLine($"qual seu sexo? M/F");
+// char resposta7 = char.Parse(Console.ReadLine());
 
-Console.WriteLine($"qual seu sexo? M/F");
-char resposta8 = char.Parse(Console.ReadLine());
+// Console.WriteLine($"qual seu sexo? M/F");
+// char resposta8 = char.Parse(Console.ReadLine());
 
-Console.WriteLine($"qual seu sexo? M/F");
-char resposta9 = char.Parse(Console.ReadLine());
+// Console.WriteLine($"qual seu sexo? M/F");
+// char resposta9 = char.Parse(Console.ReadLine());
 
-Console.WriteLine($"qual seu sexo? M/F");
-char resposta10 = char.Parse(Console.ReadLine());
-
-
-
-
-Console.WriteLine($"gostou do produto? S/N");
-char respostas1 = char.Parse(Console.ReadLine());
-
-Console.WriteLine($"gostou do produto? S/N");
-char respostas2 = char.Parse(Console.ReadLine());
-
-Console.WriteLine($"gostou do produto? S/N");
-char respostas3 = char.Parse(Console.ReadLine());
-
-Console.WriteLine($"gostou do produto? S/N");
-char respostas4 = char.Parse(Console.ReadLine());
-
-Console.WriteLine($"gostou do produto? S/N");
-char respostas5 = char.Parse(Console.ReadLine());
-
-Console.WriteLine($"gostou do produto? S/N");
-char respostas6 = char.Parse(Console.ReadLine());
-
-Console.WriteLine($"gostou do produto? S/N");
-char respostas7 = char.Parse(Console.ReadLine());
-
-Console.WriteLine($"gostou do produto? S/N");
-char respostas8 = char.Parse(Console.ReadLine());
-
-Console.WriteLine($"gostou do produto? S/N");
-char respostas9 = char.Parse(Console.ReadLine());
-
-Console.WriteLine($"gostou do produto? S/N");
-char respostas10 = char.Parse(Console.ReadLine());
+// Console.WriteLine($"qual seu sexo? M/F");
+// char resposta10 = char.Parse(Console.ReadLine());
 
 
 
 
+// Console.WriteLine($"gostou do produto? S/N");
+// char respostas1 = char.Parse(Console.ReadLine());
+
+// Console.WriteLine($"gostou do produto? S/N");
+// char respostas2 = char.Parse(Console.ReadLine());
+
+// Console.WriteLine($"gostou do produto? S/N");
+// char respostas3 = char.Parse(Console.ReadLine());
+
+// Console.WriteLine($"gostou do produto? S/N");
+// char respostas4 = char.Parse(Console.ReadLine());
+
+// Console.WriteLine($"gostou do produto? S/N");
+// char respostas5 = char.Parse(Console.ReadLine());
+
+// Console.WriteLine($"gostou do produto? S/N");
+// char respostas6 = char.Parse(Console.ReadLine());
+
+// Console.WriteLine($"gostou do produto? S/N");
+// char respostas7 = char.Parse(Console.ReadLine());
+
+// Console.WriteLine($"gostou do produto? S/N");
+// char respostas8 = char.Parse(Console.ReadLine());
+
+// Console.WriteLine($"gostou do produto? S/N");
+// char respostas9 = char.Parse(Console.ReadLine());
+
+// Console.WriteLine($"gostou do produto? S/N");
+// char respostas10 = char.Parse(Console.ReadLine());
 
 
+
+
+char sexo;
+char gostouDoProduto;
+
+int totalSim = 0;
+int totalNao = 0;
+
+int totalMulher = 0;
+int totalMulherSim = 0;
+
+int totalHomem = 0;
+int totalHomemNao = 0;
+
+float porcentagemHomemNao = 0;
+
+
+for (var i = 1; i <= 3; i++)
+{
+    // entrada de dados
+    Console.WriteLine(@$"
+    informe o sexo da pessoa:
+    (m) - Masculino
+    (f) - Feminino
+    ");
+
+    sexo = char.Parse(Console.ReadLine());
+    
+    
+
+
+    // processamento
+    if (sexo == 'f')
+    {
+        totalMulher++;
+    }
+    else
+    {
+        totalHomem++;
+    }
+    Console.WriteLine(@$"
+    A pessoa gostou do lancamento?
+     (s) - sim
+     (n) - nao
+    ");
+
+    gostouDoProduto = char.Parse(Console.ReadLine());
+
+    if (gostouDoProduto == 's')
+    {
+    totalSim ++;
+
+    if (sexo == 'f')
+    {
+        totalMulherSim++;
+    }
+
+    }
+    else
+    {
+        totalNao++;
+
+        if (sexo == 'm')
+        {
+            totalHomemNao++;
+        }
+    }
+    
+    
+}
+porcentagemHomemNao = ((float)totalHomemNao/(float)totalHomem)*100;
+Console.WriteLine($"A porcentagem de homens que responderam que nao gostaram e de: {porcentagemHomemNao} %");
+
+
+// Console.WriteLine($"totalMulher");
 
 
---
+++ exercicios-06-04-23/exercicio-05/Program.cs
@@ -0,0 +1,56 @@
+﻿// 5. Faça um programa que receba a idade, o peso e o sexo de 10 pessoas. Calcule e imprima:
+      
+// A.Total de Homens;      
+// B.Total de Mulheres;      
+// C.Média de idade dos Homens;      
+// D.Média de idade das mulheres.
+
+char sexo;
+int totalHomem = 0;
+int totalMulher = 0;
+float mediaIdadeMulher = 0;
+float mediaIdadeHomem = 0;
+int idadeMulher = 0;
+int IdadeHomem = 0;
+
+
+
+for (var i = 1; i <=10; i++)
+{
+  Console.WriteLine(@$"
+    informe o sexo da pessoa:
+    (m) - Masculino
+    (f) - Feminino
+    ");
+
+    sexo = char.Parse(Console.ReadLine());
+
+    if (sexo == 'f')
+    {
+        totalMulher++;
+    }
+    
+    if (sexo == 'm')
+    {
+        totalHomem++;
+
+        Console.WriteLine($"informe a idade");
+        IdadeHomem = int.Parse(Console.ReadLine());
+        
+    }
+    // Console.WriteLine($"informe o peso");
+    // float.Parse(Console.ReadLine());
+
+    
+    
+
+}
+mediaIdadeHomem = (IdadeHomem++ /IdadeHomem++);
+   
+//     porcentagemHomemNao = ((float)totalHomemNao/(float)totalHomem)*100;
+// Console.WriteLine($"A porcentagem de homens que responderam que nao gostaram e de: {porcentagemHomemNao} %");
+
+
+    
+    
+
