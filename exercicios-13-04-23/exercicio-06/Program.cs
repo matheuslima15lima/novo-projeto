932c4ac0a17585a93da037fa131f2172e80fdc3f
Matheus Lima Cavalcanti <46447568812@info.local>
2023-04-17 15:09:13 -0300

    finalizadas as atividades

--- exercicios-13-04-23/exercicio-06/Program.cs
+++ exercicios-13-04-23/exercicio-06/Program.cs
@@ -2,10 +2,46 @@
 // lidos em um vetor. Após isto, o algoritmo deve permitir a leitura de mais 1 nome qualquer de
 // pessoa (para efetuar uma busca) e depois escrever a mensagem ACHEI, se o nome estiver
 // entre os 10 nomes lidos anteriormente (guardados no vetor), ou NÃO ACHEI caso contrário.
-string[] nomes = new string[5];
-int[] idades = new int[5];
-for (var i = 0; i <=10; i++)
+
+//declarando um array
+string[] nomes = new string[3];
+
+
+bool nomeEncontrado = false;
+
+for (var i = 0; i < nomes.Length; i++)
 {
        Console.WriteLine($"informe o {i+1}º nome");
     nomes[i] = Console.ReadLine();
 }
+
+Console.WriteLine($"informe o nome a ser buscado: ");
+string nomeBuscado = Console.ReadLine();
+
+// processamento
+foreach ( string nome  in nomes)
+{
+    if (nome == nomeBuscado)
+    {
+        nomeEncontrado = true;
+        break;
+    }
+}
+
+// saida - exibir
+
+if (nomeEncontrado == true)
+{
+    Console.WriteLine($"ACHEI");
+    
+}
+
+else
+{
+    Console.WriteLine($"NAO ACHEI");
+    
+}
+
+string resultadoBusca = nomeEncontrado == true ? "Achei" : "NÃO ACHEI";
+
+Console.WriteLine(nomeEncontrado ? "Achei" : "nao achei");
---
+++ exercicios-13-04-23/exercicio-07/Program.cs
@@ -0,0 +1,16 @@
+﻿// Faça um algoritmo para ler 15 números e armazenar em um vetor. Após a leitura total dos
+// 15 números, o algoritmo deve escrever esses 15 números lidos na ordem inversa da qual foi
+// declarado.
+int[] numeros = new int[15];
+for (var i = 0; i < 15; i++)
+{
+    Console.WriteLine($"informe o {i+1}º numero");
+    numeros[i] = int.Parse(Console.ReadLine());
+    
+}
+
+foreach (var item in numeros.Reverse())
+{
+    Console.WriteLine($"ordem inversa: {item}");
+    
+}
