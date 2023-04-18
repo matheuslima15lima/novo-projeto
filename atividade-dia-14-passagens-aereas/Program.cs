47f4667b8d45ea6a913f91e04bd5b5b9c0897300
Matheus Lima Cavalcanti <46447568812@info.local>
2023-04-18 17:22:10 -0300

    finalizada atividade de hj

--- atividade-dia-14-passagens-aereas/Program.cs
+++ atividade-dia-14-passagens-aereas/Program.cs
@@ -14,74 +14,178 @@
 
 
 
-string senhaCorreta = "podeser";
-
-Console.WriteLine($"digite a senha");
-string senha = Console.ReadLine();
+// string senhaCorreta = "podeser";
 
 
+// Console.WriteLine($"digite a senha");
+// string senha = Console.ReadLine();
 
-while (senha != "podeser")
+
+// while (senha != "podeser")
+// {
+//     Console.WriteLine($"erro senha incorreta");
+
+// }
+
+
+
+
+
+
+
+
+static bool login(string senha)
 {
-    Console.WriteLine($"erro senha incorreta");
+    if (senha == "podeser")
+    {
+        return true;
+    }
+    else
+    {
+        Console.WriteLine($"senha incorreta!");
 
+        return false;
+    }
 }
 
 
 
 
+//declarar variaveis
+string[] nomes = new string[2];
+string[] origens = new string[2];
+string[] destinos = new string[2];
+DateOnly[] datas = new DateOnly[2];
 
+bool senhaValida;
 
-
-
-static string destiny(string destinos)
+do
 {
-    string r = destinos;
-    return r;
+    Console.WriteLine($"digite a senha");
+    string senha = Console.ReadLine();
+
+    senhaValida = login(senha);
+
+} while (senhaValida != true);
+
+
+//criar menu de opcoes
+Console.WriteLine($"menu de opcoes");
+Console.WriteLine($@"
+
+selecione uma das opcoes
+[1] - Cadastrar
+[2] - Listar
+[0] - Sair
+");
+
+string opcao = Console.ReadLine();
+
+switch (opcao)
+{
+    case "1":
+    string resposta = "";
+        do
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine($"informe o nome");
+                nomes[i] = Console.ReadLine();
+
+                Console.WriteLine($"informe seu pais de origem: ");
+                origens[i] = Console.ReadLine();
+
+                Console.WriteLine($"informe seu destino: ");
+                destinos[i] = Console.ReadLine();
+
+                Console.WriteLine($"informe a data: ");
+                datas[i] = DateOnly.Parse(Console.ReadLine());
+                Console.WriteLine($"deseja cadastrar mais uma passagem? S/N");
+                resposta = Console.ReadLine().ToLower();
+            }
+        } while (resposta == "s");
+        break;
+        case "2":
+            for (var i = 0; i < 2; i++)
+            {
+                Console.WriteLine(@$"
+                *******************
+                Passagens - Bilhete
+
+                Nome: {nomes[i]}
+                Origem: {origens[i]}
+                Destino: {destinos[i]}
+                Data: {datas[i]}
+                ");
+            }
+                break;
+    
+            case "0":
+            Console.WriteLine($"Fim do programa");
+            break;
+        default:
+            Console.WriteLine($"Opção inválida!");
+            
+        break;
+    
 }
 
 
 
-// criando os arrays
-string[] nomes = new string[5];
-string[] origens = new string[5];
-string[] destinos = new string[5];
-DateOnly[] datas = new DateOnly[5];
+// inicioCadastro:
+// for (int i = 0; i < 2; i++)
+// {
+//     Console.WriteLine($"informe seu nome ");
+//     string nm = Console.ReadLine();
 
-for (int i = 0; i < 5; i++)
-{
-    Console.WriteLine($"informe seu nome ");
-    string nm = Console.ReadLine();
+//     Console.WriteLine($"informe seu pais de origem: ");
+//     string or = Console.ReadLine();
 
-    Console.WriteLine($"informe seu pais de origem: ");
-    string or = Console.ReadLine();
+//     Console.WriteLine($"informe seu destino: ");
+//     string dn = Console.ReadLine();
 
-    Console.WriteLine($"informe seu destino: ");
-    string dn = Console.ReadLine();
-
-    Console.WriteLine($"informe a data: ");
-    DateOnly dt = DateOnly.Parse(Console.ReadLine());
+//     Console.WriteLine($"informe a data: ");
+//     DateOnly dt = DateOnly.Parse(Console.ReadLine());
 
 
-    nomes[i] = nm;
-    origens[i] = or;
-    destinos[i] = dn;
-    datas[i] = dt;
+//     nomes[i] = nm;
+//     origens[i] = or;
+//     destinos[i] = dn;
+//     datas[i] = dt;
 
-}
+// }
 
-for (int i = 0; i < 5; i++)
-{
-    Console.WriteLine(@$"
-    
-    nome: nomes {i}
-    
-    
-    
-    
-    ");
-    
-}
+// Console.WriteLine($"deseja cadastrar mais uma passagem? S/N");
+// char opcao = char.Parse(Console.ReadLine());
+
+// if (opcao == 's')
+// {
+//     goto inicioCadastro;
+// }
+
+
+
+
+// cadastrarPassagens(nomes, origens, destinos, 0);
+// cadastrarPassagens(nomes, origens, destinos, 1);
+// cadastrarPassagens(nomes, origens, destinos, 2);
+
+// for (int i = 0; i < nomes.Length; i++)
+// {
+//     Console.WriteLine($"Nome: {nomes[i]}");
+//     Console.WriteLine($"Origem: {origens[i]}");
+//     Console.WriteLine();
+
+// }
+
+
+// static void cadastrarPassagens(string[] nm, string[] or, string[] dn, string dt, int pos)
+// {
+//     nm[pos] = "{nm}";
+//     or[pos] = "{or}";
+//     dn[pos] = "{dn}";
+// }
+
 
 
 
