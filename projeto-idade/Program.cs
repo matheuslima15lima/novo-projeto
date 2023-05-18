5b0c55526b922241bff1e2bb45ceb5a55ac4aec3
Matheus Lima Cavalcanti <46447568812@info.local>
2023-05-18 14:57:10 -0300

    codigo descomentado

--- projeto-idade/Program.cs
+++ projeto-idade/Program.cs
@@ -1,25 +1,24 @@
 ﻿
-// Console.WriteLine($"Informe a sua idade: ");
-// int idade = int.Parse(Console.ReadLine());
+Console.WriteLine($"Informe a sua idade: ");
+int idade = int.Parse(Console.ReadLine());
 
-// int mes = idade * 12;
-// int dias = idade * 365;
-// int horas = dias * 24;
-// int minutos = horas * 60;
+int mes = idade * 12;
+int dias = idade * 365;
+int horas = dias * 24;
+int minutos = horas * 60;
 
-// Console.WriteLine($"A idade em mes e : {mes}, em dias e igual a : {dias}, em horas igual a : {horas} e em minuto igual a {minutos}");
-
-
+Console.WriteLine($"A idade em mes e : {mes}, em dias e igual a : {dias}, em horas igual a : {horas} e em minuto igual a {minutos}");
 
 
-// Console.WriteLine($"informe sua ano de nascimento");
-// int anoNascimento = int.Parse(Console.ReadLine());
 
-// int anoAtual = DateTime.Now.Year;
 
-// int idade = anoAtual - anoNascimento;
+Console.WriteLine($"informe sua ano de nascimento");
+int anoNascimento = int.Parse(Console.ReadLine());
 
-// Console.WriteLine($"a idade e {idade} anos");
+int anoAtual = DateTime.Now.Year;
+ idade = anoAtual - anoNascimento;
+
+Console.WriteLine($"a idade e {idade} anos");
 
 
 
