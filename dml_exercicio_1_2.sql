7482e834738bcde67571be8eb55a75e9e5b8fdd8
Matheus <matheus.cavalcanti5@aluno.senai.br>
2023-08-03 15:16:02 -0300

    adaicionar ddl

---
+++ dml_exercicio_1_2.sql
@@ -0,0 +1,22 @@
+USE Exercicio_1_2;
+
+INSERT INTO Empresa(Nome)
+Values('eff')
+
+INSERT INTO Marca(Nome)
+Values ('BMW')
+
+INSERT INTO Cliente(Nome,Cpf)
+VALUES ('Chris Bumstead','0000123323')
+
+INSERT INTO Modelo(Nome)
+VALUES ('gtx')
+
+INSERT INTO Veiculo(Placa,idMarca,IdModelo,IdEmpresa)
+VALUES()
+
+INSERT INTO Aluguel (IdVeiculo,IdCliente)
+values()
+
+select * from Veiculo
+
